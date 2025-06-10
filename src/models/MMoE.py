a65ff585ecc86a209e8254a6a44af0a32840d1b6
Hongyang Lei <166791286+HongyangLL@users.noreply.github.com>
2025-06-10 09:38:36 +0800

    Update MMoE.py

--- src/models/MMoE.py
+++ src/models/MMoE.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Hongyang Lei
+# Copyright 2025 Geely AI Lab
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
